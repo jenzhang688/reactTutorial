1fe0d28ffb51d38c5fe092adf00b0c080ec5e322
Yan Zhang <jenzhang688@gmail.com>
2024-03-10 15:45:04 -0700

    run prettier

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,15 +1,15 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
     });
-    console.log("web-vitals")
+    console.log("web-vitals");
   } else {
-    console.log("Not called")
+    console.log("Not called");
   }
 };
 
